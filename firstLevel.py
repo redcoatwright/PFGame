8afdd9f89dee18d3ec0c45ce96fac06151cfa487
Wright <redcoat@bu.edu>
2018-08-06 18:13:01 -0400

    More conditional tree/ general updates
    Fixed the conditional tree again, added some catch all conditions. Added some ascii art for fun and formatting

--- firstLevel.py
+++ firstLevel.py
@@ -5,27 +5,41 @@ def userInput(user_input):
     return user_input.lower().split(" ")
 
 def main():
-    print("You are about to embark on an epic adventure, full of magic, fighting and intrigue.")
+
+    print(" ____       _   _      __ _           _           ")
+    print("|  _ \ __ _| |_| |__  / _(_)_ __   __| | ___ _ __ ")
+    print("| |_) / _` | __| '_ \| |_| | '_ \ / _` |/ _ \ '__|")
+    print("|  __/ (_| | |_| | | |  _| | | | | (_| |  __/ |   ")
+    print("|_|   \__,_|\__|_| |_|_| |_|_| |_|\__,_|\___|_|   ")
+
     print()
+    print("You are about to embark on an epic adventure, full of magic, fighting and intrigue. \n")
     print("First, however, you have to shake a hangover, figure out how to be a person again")
-    print("and determine your path to said adventure.")
+    print("and determine your path to said adventure. \n")
     print()
-    print("It begins in darkness.")
-    print()
-    print("-------------------------------------------------------------------------------------------")
+    print("It begins in darkness...")
+
+    print("            _")
+    print(" _         | |")
+    print("| | _______| |---------------------------------------------\ ")
+    print("|:-)_______|==[]============================================>")
+    print("|_|        | |---------------------------------------------/")
+    print("           |_|")
+
     print()
 
-    print("You open your eyes. You're lying on the ground.")
-    print("You plumb the depths of what memories you have left for your name, you find it.")
+    print("You open your eyes. You're lying on the ground. \n")
+    print("You plumb the depths of what memories you have left for your name, you find it. \n")
 
-    name = input("What is your name? If you don't answer, it'll be set to a default name which is pretty stupid so you may as well put something down... ")
-
+    name = input("What is your name? If you don't answer, it'll be set to a default name which is pretty stupid so you may as well put something down...  \n \nEnter your name: ")
+    print("\n")
+    
     player = playerChar.newPC()
 
     if name != "":
         player.change_name(name)
 
-    print(player.char_name)
+    print("Hi " + player.char_name + "\n")
     
     control_signal_1 = 0
     control_signal_2 = 0
@@ -38,105 +52,179 @@ def main():
 
     while loop_control == 0:
 
-        user_input = userInput(input("What do you want to do?"))
+        print("What do you want to do?")
+        
+        user_input = userInput(input("\n >>> "))
 
+        print()
+        
         [x.lower() for x in user_input]
 
         if len(user_input) == 1:
             
             if user_input[0] == "look":
-                print("Look where? Try saying 'look left' or 'look right'")
+                print("Look where? Try saying 'look left' or 'look right' \n")
 
             elif user_input[0] == "search":
-                print("Search where, bud?")
+                print("Search where, bud? \n")
 
             elif user_input[0] == "take":
-                print("Take what...? Try again, this time with an object in mind")
+                print("Take what...? Try again, this time with an object in mind \n")
 
             elif user_input[0] == "stand":
-                print("You are currently standing up, congratulations on figuring out basic motor control, pssshhh, just fyi, babies do that every day. Baby.")
+                print("You are currently standing up, congratulations on figuring out basic motor control, pssshhh, just fyi, babies do that every day. Baby. \n")
                 control_signal_3 = 1 #the control signal that allows the player to fight further on in level 1'
 
             elif user_input[0] == "shout" and user_input[0] == "yell" and user_input[0] == "scream":
-                print("You shout into the darkness, your voice echoes back at you from the dark.")
+                print("You shout into the darkness, your voice echoes back at you from the dark. \n")
 
             elif user_input[0] == "help":
-                print("Here are some commands you can use 'Look (left)', 'Search (the ground)' or look/search other places")
+                print("Here are some commands you can use 'Look (left)', 'Search (the ground)' or look/search other places \n")
 
-        else:
+            else:
+                print("What the hell! You didn't say anything that makes sense! Try again, dummy. \n")
+
+        elif len(user_input) == 2:
 
             if user_input[0] == "look":
                 if user_input[1] == "left":
-                    print("You see darkness...")
+                    print("You see darkness... \n")
                     counter += 1
                     if counter == 3:
-                        print("Huh, it's almost like there's darkness in ALL directions, weird... but definitely keep trying")
+                        print("Huh, it's almost like there's darkness in ALL directions, weird... but definitely keep trying \n")
                     elif counter == 5:
-                        print("SERIOUSLY, MAYBE TRY SOMETHING ELSE, 'HELP' MIGHT BE UP YOUR ALLEY")
+                        print("SERIOUSLY, MAYBE TRY SOMETHING ELSE, 'HELP' MIGHT BE UP YOUR ALLEY \n")
                     
                 elif user_input[1] == "right":
-                    print("You see darkness...")
+                    print("You see darkness... \n")
                     counter += 1
                     if counter == 3:
-                        print("Huh, it's almost like there's darkness in ALL directions, weird... but definitely keep trying")
+                        print("Huh, it's almost like there's darkness in ALL directions, weird... but definitely keep trying \n")
                     elif counter == 5:
-                        print("SERIOUSLY, MAYBE TRY SOMETHING ELSE, 'HELP' MIGHT BE UP YOUR ALLEY")
+                        print("SERIOUSLY, MAYBE TRY SOMETHING ELSE, 'HELP' MIGHT BE UP YOUR ALLEY \n")
                     
                 elif user_input[1] == "up":
-                    print("You see darkness...")
+                    print("You see darkness... \n")
                     counter += 1
                     if counter == 3:
-                        print("Huh, it's almost like there's darkness in ALL directions, weird... but definitely keep trying")
+                        print("Huh, it's almost like there's darkness in ALL directions, weird... but definitely keep trying \n")
                     elif counter == 5:
-                        print("SERIOUSLY, MAYBE TRY SOMETHING ELSE, 'HELP' MIGHT BE UP YOUR ALLEY")
+                        print("SERIOUSLY, MAYBE TRY SOMETHING ELSE, 'HELP' MIGHT BE UP YOUR ALLEY \n")
                     
                 elif user_input[1] == "down":
-                    print("You see darkness...")
+                    print("You see darkness... \n")
                     counter += 1
                     if counter == 3:
-                        print("Huh, it's almost like there's darkness in ALL directions, weird... but definitely keep trying")
+                        print("Huh, it's almost like there's darkness in ALL directions, weird... but definitely keep trying \n")
                     elif counter == 5:
-                        print("SERIOUSLY, MAYBE TRY SOMETHING ELSE, 'HELP' MIGHT BE UP YOUR ALLEY")
+                        print("SERIOUSLY, MAYBE TRY SOMETHING ELSE, 'HELP' MIGHT BE UP YOUR ALLEY \n")
                     
                 else:
-                    print("Huh, you said 'look {0}' which either doesn't make sense or isn't a viable command, try again".format(user_input[1]))
+                    print("Huh, you said 'look {0}' which either doesn't make sense or isn't a viable command, try again \n".format(user_input[1]))
 
             elif user_input[0] == "search":
-                if user_input[1] == "ground" or user_input[2] == "ground":
-                    print("You grope around on the ground and your hands stumble across a large rock, it could be used as an impromptu weapon...")
+
+                if user_input[1] == "ground":
+
+                    print("You grope around on the ground and your hands stumble across a large rock, it could be used as an impromptu weapon... \n")
                     control_signal_1 = 1
 
-                elif user_input[1] == "pockets" or user_input[2] == "pockets" or user_input[1] == "pocket" or user_input[2] == "pocket" or user_input[1] == "self" or user_input[1] == "myself":
+                elif user_input[1] == "pockets" or user_input[1] == "pocket" or user_input[1] == "self" or user_input[1] == "myself":
+
                     print("You search in all your pockets and luckily find a box of tinder and a flint. You don't remember putting it there, but then again, you don't remember anything pretty much.")
-                    print("Classic amnesia...")
+                    print("Classic amnesia... \n")
                     control_signal_2 = 1
 
-                elif user_input[1] == "memories" or user_input[2] == "memories" or user_input[1] == "memory" or user_input[2] == "memory":
-                    print("You don't remember anything other than the clink of glasses and the dull roar of a crowded tavern. That explains the pounding headache and the nausea. That's no good.")
+                elif user_input[1] == "memories" or user_input[1] == "memory":
+
+                    print("You don't remember anything other than the clink of glasses and the dull roar of a crowded tavern. That explains the pounding headache and the nausea. That's no good. \n")
 
                 else:
-                    print("Search where now?? You said 'search {0}' but that really doesn't make sense or maybe we didn't program that response, try something else, hoss.".format(user_input[1]))
+
+                   print("Search where now?? I didn't understand what you said \n")
 
             elif user_input[0] == "take":
-                if (user_input[1] == "tinder" or (user_input[1] + user_input[2]) == "tinderbox" or user_input[1] == "tinderbox" or user_input[1] == "box" or user_input[1] == "flint") \
-                    and control_signal_2 == 1:
-                    print("You take the tinder box and flint, you little pyro, you")
+
+                if (user_input[1] == "tinder" or user_input[1] == "tinderbox" or user_input[1] == "box" or user_input[1] == "flint") and control_signal_2 == 1:
+
+                    print("You take the tinder box and flint, you little pyro, you \n")
                     control_signal_2 = 0
                     player.inventory.append("tinder box and flint")
 
-                elif (user_input[1] == "rock" or user_input[2] == "rock") and control_signal_1 == 1:
-                    print("You grab the rock, heft it, good weight to it. You stick it in your backpack in case you need it later.")
+                elif user_input[1] == "rock" and control_signal_1 == 1:
+
+                    print("You grab the rock, heft it, good weight to it. You stick it in your backpack in case you need it later. \n")
                     control_signal_1 = 0
                     player.inventory.append("rock")
 
-                elif (user_input[1] + user_input[2] + user_input[3] + user_input[4]) == "stockofthesituation":
-                    print("Well it isn't great...you've woken up in god only knows where, splitting headache and what's the weird noise you're hearing...")
 
             elif (user_input[0] + user_input[1]) == "getup":
-                print("You are currently standing up, congratulations on figuring out basic motor control, pssshhh, just fyi, babies do that every day. Baby.")
+
+                print("You are currently standing up, congratulations on figuring out basic motor control, pssshhh, just fyi, babies do that every day. Baby. \n")
                 control_signal_3 = 1 #the control signal that allows the player to fight further on in level 1'
+
+            else:
+
+                print("What the hell! You didn't say anything that makes sense! Try again, dummy. \n")
         
-       
+        elif len(user_input) == 3:
+
+            if user_input[0] == "search":
+
+                if user_input[2] == "ground":
+
+                    print("You grope around on the ground and your hands stumble across a large rock, it could be used as an impromptu weapon... \n")
+                    control_signal_1 = 1
+
+                elif user_input[2] == "pockets" or user_input[2] == "pocket":
+
+                    print("You search in all your pockets and luckily find a box of tinder and a flint. You don't remember putting it there, but then again, you don't remember anything pretty much.")
+                    print("Classic amnesia... \n")
+                    control_signal_2 = 1
+
+                elif user_input[2] == "memories" or user_input[2] == "memory":
+
+                    print("You don't remember anything other than the clink of glasses and the dull roar of a crowded tavern. That explains the pounding headache and the nausea. That's no good. \n")
+
+                else:
+                    
+                    print("Search where now?? I didn't understand what you said \n")
+
+            elif user_input[0] == "take":
+
+                if (user_input[1] + user_input[2]) == "tinderbox" and control_signal_2 == 1:
+
+                    print("You take the tinder box and flint, you little pyro, you \n")
+                    control_signal_2 = 0
+                    player.inventory.append("tinder box and flint")
+
+                elif user_input[2] == "rock" and control_signal_1 == 1:
+
+                    print("You grab the rock, heft it, good weight to it. You stick it in your backpack in case you need it later. \n")
+                    control_signal_1 = 0
+                    player.inventory.append("rock")
+
+                else:
+                    
+                    print("Take what now? I don't understand what you said \n")
+
+            else:
+
+                print("What the hell! You didn't say anything that makes sense! Try again, dummy. \n")
+                
+        elif len(user_input) == 4:
+
+            if user_input[0] == "take":
+                
+                if (user_input[1] + user_input[2] + user_input[3] + user_input[4]) == "stockofthesituation":
+
+                    print("Well it isn't great...you've woken up in god only knows where, splitting headache and what's the weird noise you're hearing... \n")
+
+
+        else:
+
+            print("What the hell! You didn't say anything that makes sense! Try again, dummy. \n")
+        
 
         if "tinder box and flint" in player.inventory and control_signal_3 == 1:
             
