273c7ff029479e67124c146ca355195c131b96be
redcoatwright <Redcoatwright@gmail.com>
2018-09-06 21:53:02 -0400

    split playerChar
    split playerChar and added ability scores and the ability to assign them manually or randomly

--- player/playerData.py
+++ player/playerData.py
@@ -1,8 +1,9 @@
 import data.weaponList
 import player.playerMechs
 
+mechs = player.playerMechs
 
-class newPC():
+class NewPC():
 
     char_name = "Defaulty McDefaultNameplate"
     current_xp = 0
@@ -12,10 +13,20 @@ class newPC():
     init = 0
     melee_attk = 0
 
+    # ability scores
+    cha = 10
+    str = 10
+    wis = 10
+    con = 10
+    int = 10
+    dex = 10
+
+    # current position on the map
     pos_x = 0
     pos_y = 0
 
-    einventory = []
+    # equippable inventory and regular inventory
+    e_inventory = []
     inventory = []
 
     equItem = data.weaponList.Fists()
@@ -25,3 +36,21 @@ class newPC():
 
     def set_level(self, current_level):
         self.current_level = current_level
+
+    def set_ab_scores(self, cha, str, wis, con, int, dex):
+        self.cha = cha
+        self.str = str
+        self.wis = wis
+        self.con = con
+        self.int = int
+        self.dex = dex
+
+    def set_rand_abs(self):
+        
+        self.cha = mechs.rand_ab_score()
+        self.str = mechs.rand_ab_score()
+        self.wis = mechs.rand_ab_score()
+        self.con = mechs.rand_ab_score()
+        self.int = mechs.rand_ab_score()
+        self.dex = mechs.rand_ab_score()
+
--- player/playerMechs.py
+++ player/playerMechs.py
@@ -1,95 +1,114 @@
-def move(self, noun):
+import mechanics.diceRolling
+
+mechs = mechanics.diceRolling
+
+def move(new_pc, noun):
     if not noun:
         print("Move where?")
     elif noun == 'north':
-        self._move_north()
+        _move_north()
     elif noun == 'south':
-        self._move_south()
+        _move_south()
     elif noun == 'east':
-        self._move_east()
+        _move_east()
     elif noun == 'west':
-        self._move_west()
+        _move_west()
     else:
         print("I don't understand that direction. Try again.")
 
-    print("You are currently at: ", self.pos_x, self.pos_y)
-    print(self.current_level.level[self.pos_x][self.pos_y].intro_text)
+    print("You are currently at: ", new_pc.pos_x, new_pc.pos_y)
+    print(new_pc.current_level.level[new_pc.pos_x][new_pc.pos_y].intro_text)
 
 
-def fight(self, noun):
-    didFight = False
-    for enemy in self.current_level.level[self.pos_x][self.pos_y].enemies:
+def fight(new_pc, noun):
+    did_fight = False
+    for enemy in new_pc.current_level.level[new_pc.pos_x][new_pc.pos_y].enemies:
         if enemy.cmdName == noun:
             print("You fight the %s. Good work." % noun)
-            didFight = True
+            did_fight = True
             break
-    if not didFight:
+    if not did_fight:
         print("There's no %s here to fight." % noun)
 
 
-def _move_self(self, dx, dy):
-    newX = self.pos_x + dx
-    newY = self.pos_y + dy
+def _move_self(new_pc, dx, dy):
+    new_x = new_pc.pos_x + dx
+    new_y = new_pc.pos_y + dy
 
-    if self.current_level.is_valid_area(newX, newY):
-        self.pos_x = newX
-        self.pos_y = newY
+    if new_pc.current_level.is_valid_area(new_x, new_y):
+        new_pc.pos_x = new_x
+        new_pc.pos_y = new_y
     else:
-        self.current_level.print_invalid_area()
+        new_pc.current_level.print_invalid_area()
 
 
-def _move_north(self):
-    self._move_self(-1, 0)
+def _move_north(new_pc):
+    _move_self(new_pc, -1, 0)
 
 
-def _move_south(self):
-    self._move_self(1, 0)
+def _move_south(new_pc):
+    _move_self(new_pc, 1, 0)
 
 
-def _move_east(self):
-    self._move_self(0, 1)
+def _move_east(new_pc):
+    _move_self(new_pc, 0, 1)
 
 
-def _move_west(self):
-    self._move_self(0, -1)
+def _move_west(new_pc):
+    _move_self(new_pc, 0, -1)
 
 
-def equipItem(self, item):
-    newPC.equItem = item
+def equipItem(new_pc, item):
+    new_pc.equItem = item
 
 
-def add_to_inv(self, item):
-    newPC.inventory.append(item)
+def add_to_inv(new_pc, item):
+    new_pc.inventory.append(item)
 
 
-def add_xp(self, amt):
-    newPC.current_xp += amt
+def add_xp(new_pc, amt):
+    new_pc.current_xp += amt
 
 
-def sub_hp(self, amt):
-    newPC.c_hp -= amt
+def sub_hp(new_pc, amt):
+    new_pc.c_hp -= amt
 
 
-def add_hp(self, amt):
-    newPC.c_hp += amt
+def add_hp(new_pc, amt):
+    new_pc.c_hp += amt
 
 
-def add_tot_hp(self, amt):
-    newPC.t_hp += amt
+def add_tot_hp(new_pc, amt):
+    new_pc.t_hp += amt
 
 
-def add_ac(self, amt):
-    newPC.ac += amt
+def add_ac(new_pc, amt):
+    new_pc.ac += amt
 
 
-def mel_atk(self, amt):
-    newPC.melee_attk += amt
+def mel_atk(new_pc, amt):
+    new_pc.melee_attk += amt
 
 
-def add_init(self, amt):
-    newPC.init += amt
+def add_init(new_pc, amt):
+    new_pc.init += amt
 
 
-def change_name(self, name):
-    newPC.char_name = name
+def change_name(new_pc, name):
+    new_pc.char_name = name
+
+def rand_ab_score():
+    rolls = []
+
+    for i in range(6):
+        rolls.append(mechs.roll_d6())
+
+    rolls.sort(reverse=True)
+
+    total = 0
+
+    for i in range(4):
+        total += rolls[i]
+
+    return total
 
