dc796d62a0fa66849c8837fa96e33c4e0d80ba0c
redcoatwright <Redcoatwright@gmail.com>
2020-01-17 14:08:11 -0500

    Update playerActs.py

--- mechanics/playerActs.py
+++ mechanics/playerActs.py
@@ -1,1 +1,27 @@
 # File that contains all of the basic player actions aka ones that the player can ALWAYS do unless restricted by something
+"""
+Feature List:
+- Look
+    -West
+    -East
+    -Up
+    -Down
+    -North
+    -South
+
+- Move
+    -South
+    -North
+    -East
+    -West
+
+- Search the ground
+- Skills tests
+    -perception
+    -bluff
+    -persuade
+    -strength
+    -acrobatics
+    -
+"""
+
